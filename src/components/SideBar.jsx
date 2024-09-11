8e700f362439fb496cc842a6bf3a768e619e7dd2
FredyAlexHoyosAriza <donotgobehind@gmail.com>
2024-09-10 21:00:37 -0500

    Se crea menu con estilos en el sidebar con Links a rutas administrativas

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SideBar = () => {
   return (
     //w-72: la mitad de una pantalla mediana
     <nav className='w-72 bg-zinc-500 '>
-      SideBar
+      <ul className='box-border p-2 ml-1 font-bold text-lg text-cyan-200 h-screen flex flex-col justify-around'>
+        <li>
+          <Link to={'/admin/vehicles'}>Gestionar vehiculos</Link>
+        </li>
+        <li>
+          <Link to={'/admin/clients'}>Gestionar clientes</Link>
+        </li>
+        <li>
+          <Link to={'/admin'}>Inicio panel administración</Link>
+        </li>
+      </ul>
     </nav>
   )
 }
